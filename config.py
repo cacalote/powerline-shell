a292e63ff7b76b1b97ea4159ad4cba6b990f8d81
Matías Aguirre <matiasaguirre@gmail.com>
2014-07-14 21:20:08 -0300

    Simple cwd segment that shows last three directories

---
+++ config.py
@@ -0,0 +1,24 @@
+# This is the configuration file for your powerline-shell prompt
+# Every time you make a change to this file, run install.py to apply changes
+#
+# For instructions on how to use the powerline-shell.py script, see the README
+
+# Add, remove or rearrange these segments to customize what you see on the
+# shell prompt. Any segment you add must be present in the segments/ directory
+
+SEGMENTS = [
+    'jobs',
+    'time',
+    'hostname',
+    'virtual_env',
+    'ssh',
+    'simple_cwd',
+    'git',
+    'hg',
+    'svn',
+    'read_only',
+    'root',
+]
+
+# Change the colors used to draw individual segments in your prompt
+THEME = 'default'
